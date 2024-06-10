65bf11a1225fdccac199b5cf6ba5fab699b0e6f3
colissi04 <rcolissi@gmail.com>
2024-06-10 15:53:14 -0300

    adicionado remover turma

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,18 @@
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { Container } from "./styles";
 import { useTheme } from "styled-components/native";
 
 type Props = TextInputProps & {
   placeholder: string;
+  inputRef?: React.RefObject<TextInput>
 }
 
-export function Input({placeholder, ...rest}: Props){
+export function Input({placeholder, inputRef, ...rest}: Props){
   const { COLORS } = useTheme();
 
   return(
     <Container
+      ref={inputRef}
       placeholder={placeholder}
       placeholderTextColor={COLORS.GRAY_300}
       {...rest}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-import { Alert, FlatList } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
-import { useRoute } from "@react-navigation/native";
+import { Alert, FlatList, TextInput } from "react-native";
+
+import { Container, Form, HeaderList, NumberOfPlayers, PlayersContent } from "./styles";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -10,14 +12,17 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { ListEmpty } from "@components/ListEmpty";
-
-import { Container, Form, HeaderList, NumberOfPlayers, PlayersContent } from "./styles";
 import { PlayerCard } from "@components/PlayerCard";
 import { Button } from "@components/Button";
-import { AppError } from "@utils/AppError";
+
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
+import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
+import { groupRemoveByName } from "@storage/group/groupRemoveByName";
+
+import { AppError } from "@utils/AppError";
+
 
 type RouteParams = {
   group: string
@@ -28,8 +33,11 @@ export function Players(){
   const [team, setTeam] = useState<string>("Time A");
   const [players, setPlayers] = useState<PlayerStorageDTO[]>();
 
+  const navigation = useNavigation();
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  const newPlayerNameInputRef = useRef<TextInput>(null);
 
   async function handleAddPlayer(){
     if(newPlayerName.trim().length === 0) {
@@ -43,8 +51,12 @@ export function Players(){
 
     try {
       await playerAddByGroup(newPlayer, group);
+
+      newPlayerNameInputRef.current?.blur();
+
+      setNewPlayerName('');
       fetchPlayersByTeam();
-      
+
     } catch(error) {
       if (error instanceof AppError) {
         Alert.alert('Nova pessoa', error.message)
@@ -65,6 +77,37 @@ export function Players(){
     }
   }
 
+  async function handlePlayerRemove(playerName: string){
+    try{
+      await playerRemoveByGroup(playerName, group);
+      fetchPlayersByTeam();
+
+    } catch(error){
+      Alert.alert('Não foi possível remover o player escolhido.')
+    }
+  }
+
+  async function groupRemove(){
+    try {
+      await groupRemoveByName(group);
+      navigation.navigate('groups');
+
+    } catch(error){
+      Alert.alert('Não foi possível remover a turma.')
+    }
+  }
+
+  async function handleGroupRemove(){
+    Alert.alert(
+      'Remover',
+      'Deseja remover a turma?',
+      [
+        { text: 'Não', style: 'cancel'},
+        { text: 'Sim', onPress: () => groupRemove()}
+      ]
+    )
+  }
+
   useEffect(() => {
     fetchPlayersByTeam();
   }, [team]);
@@ -80,9 +123,13 @@ export function Players(){
       
       <Form>
         <Input
+          inputRef={newPlayerNameInputRef}
           onChangeText={setNewPlayerName}
+          value={newPlayerName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
         />
         <ButtonIcon
           icon="add"
@@ -114,7 +161,7 @@ export function Players(){
           renderItem={({ item }) => (
             <PlayerCard 
               player={item.name}
-              onRemove={() => {}} //falta a funcionalidade do on remove
+              onRemove={() => handlePlayerRemove(item.name)} 
             />
           )}
           ListEmptyComponent={() =>( 
@@ -130,6 +177,7 @@ export function Players(){
         <Button
           title="Remover Turma"
           type="SECONDARY"
+          onPress={handleGroupRemove}
         />
 
     </Container>
